cbd092ac9942b86560c4054305e2eb8f7fda620c
Clint Baker <clintonbaker@Clintons-iMac.local>
2018-03-12 16:46:22 -0600

    stable api

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
+const stations = require('./stations.js');
 
 const argv = yargs
   .options({
@@ -34,4 +35,23 @@ function geoLocator(address) {
   });
 }
 
-geoLocator(address);
+function oneDayAddress(address) {
+  const key = 'AIzaSyCVeCwLTOA6YU0cJI70SyO3t7W-p7ud9RM';
+  const encodedAddress = encodeURIComponent(address);
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${key}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      var lat = body.results[0].geometry.location.lat;
+      var lng = body.results[0].geometry.location.lng;
+      var address = body.results[0].formatted_address;
+      stations.closestStations(lat, lng);
+    }
+  });
+}
+
+const address = argv.a;
+oneDayAddress(address);
--- app/stations.js
+++ app/stations.js
@@ -12,7 +12,39 @@ function getStations() {
       console.log(JSON.stringify(body, undefined, 2));
     }
   });
-
 }
 
-getStations()
+
+function closestStations(lat, lng) {
+  request({
+    url: `http://api.powderlin.es/closest_stations?lat=${lat}&lng=${lng}&data=true&days=1&count=1`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      var twentyFourHourSnowfall = body[0].data[0]["Change In Snow Depth (in)"];
+
+      if (twentyFourHourSnowfall > 1) {
+
+      } else {
+        twentyFourHourSnowfall = 0;
+      }
+
+      var station = {
+        elevation: body[0].station_information.elevation,
+        name: body[0].station_information.name,
+        date: body[0].data[0]["Date"],
+        snowDepth: body[0].data[0]["Snow Depth (in)"],
+        twentyFourHourSnowfall
+      };
+      console.log(station);
+    }
+  });
+}
+
+
+module.exports = {
+  getStations,
+  closestStations
+};
